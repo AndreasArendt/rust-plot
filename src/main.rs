44ae756d615c164526e0c005781162ae0835df27
aarendt <andi.arendt@icloud.com>
2025-06-21 09:33:22 +0200

    update example

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let y: Vec<f64> = (0..100).map(f64::from).collect();
 
     let mut p = rplt::Rplot::default();
-    p.plot(x,y, Some(LineStyle::Marker(rplt::MarkerStyle::Cross)));
+    p.plot(x.clone(),y.clone(), Some(LineStyle::Marker(rplt::MarkerStyle::Cross)));
+    p.plot(x.clone(),y.clone(), Some(LineStyle::Line));
     let _ = p.show();
 }
